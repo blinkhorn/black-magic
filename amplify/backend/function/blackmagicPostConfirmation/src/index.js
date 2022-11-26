f8593c80f05da18a0ae08bda9a7c3b48360721ea
Patrick Blinkhorn <contact.blinkhorn@gmail.com>
2022-11-26 17:24:57 -0500

    logic to distribute existing mp3s to new users

--- amplify/backend/function/blackmagicPostConfirmation/src/index.js
+++ amplify/backend/function/blackmagicPostConfirmation/src/index.js
@@ -1,21 +1,75 @@
 const AWS = require('aws-sdk');
 const { v4: uuidv4 } = require('uuid');
 
-const documentClient = new AWS.DynamoDB.DocumentClient();
+const dynamoDbDocumentClient = new AWS.DynamoDB.DocumentClient();
 const s3 = new AWS.S3();
+
+exports.handler = async (event, context, callback) => {
+  try {
+    if (event.request.userAttributes['cognito:user_status'] === 'CONFIRMED') {
+      const userId = await createUser(event.request.userAttributes.email);
+      await createUserFolder(userId);
+      await distributeExistingMp3sToUser(userId);
+      // return to Cognito
+    } else {
+      // Nothing to do, the user's not confirmed
+      callback(null, event);
+    }
+    return event;
+  } catch (err) {
+    console.log(err);
+  }
+};
 
 async function createUser(email) {
   try {
     const userId = uuidv4();
-    const usersTableParams = {
+    const usersTablePutParams = {
       TableName: process.env.USERS_TABLE_NAME,
       Item: {
         email,
         id: userId
       }
     };
-    await documentClient.put(usersTableParams).promise();
+    await dynamoDbDocumentClient.put(usersTablePutParams).promise();
     return userId;
+  } catch (err) {
+    return err;
+  }
+}
+
+async function createUserFolder(userId) {
+  try {
+    const createUserFolderParams = {
+      Bucket: process.env.DESTINATION_BUCKET,
+      Key: `${userId}/`
+    };
+
+    await s3.putObject(createUserFolderParams).promise();
+  } catch (err) {
+    return err;
+  }
+}
+
+async function distributeExistingMp3sToUser(userId) {
+  try {
+    const mp3sToDistribute = [...(await listMp3sToDistribute())];
+    for (let mp3Name of mp3sToDistribute) {
+      const mp3Params = {
+        CopySource: `${process.env.SOURCE_BUCKET}/${mp3Name}.mp3`,
+        Bucket: process.env.DESTINATION_BUCKET,
+        Key: `${userId}/${mp3Name}.mp3`
+      };
+      await s3
+        .copyObject(mp3Params, function (copyErr, copyData) {
+          if (copyErr) {
+            console.log(`Error: ${copyErr}`);
+          } else {
+            console.log(`Success: ${copyData}`);
+          }
+        })
+        .promise();
+    }
   } catch (err) {
     return err;
   }
@@ -29,8 +83,8 @@ async function listMp3sToDistribute() {
     const mp3sToDistribute = [];
     let items;
     do {
-      items = await documentClient.scan(codesTableParams).promise();
-      items.Items.forEach((item) => mp3sToDistribute.push(item));
+      items = await dynamoDbDocumentClient.scan(codesTableParams).promise();
+      items.Items.forEach((item) => mp3sToDistribute.push(item.code));
       codesTableParams.ExclusiveStartKey = items.LastEvaluatedKey;
     } while (typeof items.LastEvaluatedKey !== 'undefined');
 
@@ -39,43 +93,3 @@ async function listMp3sToDistribute() {
     return err;
   }
 }
-
-async function distributeExistingMp3sToUser(userId) {
-  try {
-    const mp3sToDistribute = [...(await listMp3sToDistribute())];
-    for (let mp3 of mp3sToDistribute) {
-      s3.copyObject(
-        {
-          CopySource: `${process.env.SOURCE_BUCKET}/${mp3}`,
-          Bucket: process.env.DESTINATION_BUCKET,
-          Key: `${userId}/${mp3}`
-        },
-        function (copyErr, copyData) {
-          if (copyErr) {
-            console.log(`Error: ${copyErr}`);
-          } else {
-            console.log(`Success: ${copyData}`)
-          }
-        }
-      );
-    }
-  } catch (err) {
-    return err;
-  }
-}
-
-exports.handler = async (event, context, callback) => {
-  try {
-    if (event.request.userAttributes['cognito:user_status'] === 'CONFIRMED') {
-      const userId = await createUser(event.request.userAttributes.email);
-      await distributeExistingMp3sToUser(userId);
-      // return to Cognito
-    } else {
-      // Nothing to do, the user's not confirmed
-      callback(null, event);
-    }
-    return event;
-  } catch (err) {
-    console.log(err);
-  }
-};
